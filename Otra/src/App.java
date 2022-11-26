134d1157bb15f80a4beb2585a2242a276016f0e2
zubzer03 <leogaborojas1@gmail.com>
2022-11-26 13:02:44 -0500

    Inicio

---
+++ Otra/src/App.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        JFrame frame = new JFrame("Chat Frame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+        frame.setLayout(new BorderLayout());
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("Archivo");
+        JMenu helpMenu = new JMenu("Ayuda");
+        menuBar.add(fileMenu);
+        menuBar.add(helpMenu);
+        JMenuItem openItem = new JMenuItem ("Abrir");
+        JMenuItem saveItem = new JMenuItem ("Guardar Como");
+        fileMenu.add(openItem);
+        helpMenu.add(saveItem);
+
+        JPanel panel = new JPanel();
+        JLabel messageLabel = new JLabel("Mensaje: ");
+        JTextField chatTextField = new JTextField(10);
+        JButton sendButton = new JButton("Enviar");
+        JButton resetButton = new JButton("Restablecer");
+        panel.setLayout(new FlowLayout());
+        panel.add(messageLabel);
+        panel.add(chatTextField);
+        panel.add(sendButton);
+        panel.add(resetButton);
+
+        JTextArea chatHistory = new JTextArea();
+
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.getContentPane().add(BorderLayout.NORTH, menuBar);
+        frame.getContentPane().add(BorderLayout.CENTER, chatHistory);
+        frame.setVisible(true);
+    }
+}
---
+++ Swing/src/App.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class App {
+    
+
+    public static void main(String[] args) throws Exception {
+        JFrame frame = new JFrame("ThinkPad");
+        
+    }
+}
